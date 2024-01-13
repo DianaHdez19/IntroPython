dd3b79051897436281331a665c8ee928ad1adae1
DianaHdez19 <lr5650299@gmail.com>
2024-01-12 22:06:48 -0600

    DLHR, 11 enero 2024, Tuplas

---
+++ tema3-Funciones.py
@@ -0,0 +1,9 @@
+texto = "Universidad Tecnologica"
+print(texto.lower())
+print(texto.upper())
+print(texto.title())
+print(texto.find("de"))
+print(texto.count("e"))
+textoNnuevo = texto.replace("e", "3")
+print(textoNnuevo)
+
